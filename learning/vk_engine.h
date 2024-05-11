c7add30c502c08869bcf529f2f28b28c65340c6f
Bryan Tabinas <bryan.tabinas@gateway.one>
2024-05-11 18:59:51 +0200

    finished chapter: https://vkguide.dev/docs/new_chapter_1/vulkan_init_code/

--- learning/vk_engine.h
+++ learning/vk_engine.h
@@ -11,9 +11,22 @@ public:
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 	bool stop_rendering{ false };
-	VkExtent2D _windowExtent{ 1700 , 900 };
+	VkExtent2D _windowExtent{ 800 , 600 };
 
 	struct SDL_Window* _window{ nullptr };
+
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
 
 	static VulkanEngine& Get();
 
@@ -28,5 +41,13 @@ public:
 
 	//run main loop
 	void run();
+
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
 //< intro
